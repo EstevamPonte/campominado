365aa6852a815f32b8e5d74ba80b283ed38c908b
estevamponte <tecoponte@gmail.com>
2019-12-15 18:17:45 -0300

    terminado campominado

--- DjangoCampoMinado/projeto/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import iniciar_jogo, post_list
-
-urlpatterns = [
-    path('start/', iniciar_jogo, name='iniciar_game'),
-    path('jogadas/', post_list, name='jogar_jogo'),
-]
